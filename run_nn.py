3b6fd23c2bd3675693329e7f59f7c4509a0da5d4
林政文 <victorlin@linzhenwendeMBP.lan>
2019-08-27 03:05:37 +0800

    Add the main program "run_nn.py"

---
+++ run_nn.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+import io
+import re
+import time
+import numpy as np
+import unicodedata
+import tensorflow as tf
+
+PATH_TO_FILE = "./dataset/cmn_trad_seg_ukn.txt"
+
+def train():
+    print("train")
+    # train
+
+def translate():
+    print("translate")
+    # translate
