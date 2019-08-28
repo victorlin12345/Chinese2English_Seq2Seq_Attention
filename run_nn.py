7b8fe5db1cfd1970aeb945681a8780c1bb211749
victorlin12345 <victorlin12345@gmail.com>
2019-08-29 00:24:32 +0800

    Add load_dataset in def train()

--- run_nn.py
+++ run_nn.py
@@ -3,17 +3,31 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import os
 import io
 import re
+import sys
 import time
 import numpy as np
 import unicodedata
 import tensorflow as tf
 
+from data_loader import load_dataset
+
 PATH_TO_FILE = "./dataset/CMN_TRAD_SEG.txt"
+SAVE_DIR = 'checkpoints/training_checkpoints'
+SAVE_PATH = os.path.join(SAVE_DIR, 'ckpt')  # 最佳验证结果保存路径
 
 def train():
-    print("train")
-    # train
+    print("Loading training data...")
+    input_tensor, target_tensor, inp_lang, targ_lang = load_dataset(PATH_TO_FILE, num_examples=None)
+    print(len(input_tensor))
 
 def translate():
     print("translate")
     # translate
+
+if __name__ == '__main__':
+
+    if len(sys.argv) != 2 or sys.argv[1] not in ['train']:
+        raise ValueError("""usage: python run_nn.py [train]""")
+
+    if sys.argv[1] == 'train':
+        train()
